25ca904c75c1605d8d16951cae763d88f5da489f
johnny <vmjuan90@gmail.com>
2021-11-01 11:10:08 +0100

    small improvements

--- include/dynamic_array.h
+++ include/dynamic_array.h
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 
-struct go_array {
+struct array {
 	void *storage;
 	unsigned int length;
 	unsigned int capacity;
 	size_t element_size;
 };
 
-struct go_array *go_append(struct go_array *, const void *);
-#define go_push go_append
-struct go_array* go_make(struct go_array*, const unsigned int);
-void* go_pop(struct go_array*, void*);
+struct array_array *array_append(struct array *, const void *);
+#define array_push array_append
+struct array* array_make(struct array*, const unsigned int);
+void* array_pop(struct array*, void*);
--- src/dynamic_array.c
+++ src/dynamic_array.c
@@ -5,18 +5,19 @@
 #include "dynamic_array.h"
 
 /**
- * \brief append element to struct go_array
+ * \brief append element to struct array_array
  *
  * the expected size of new_element MUST be equal to data_size,
  * \param arr handled array
  * \param new_element new element to append to arr
  * \return null if error else pointer to arr
  */
-struct go_array *go_append(struct go_array *arr, const void *new_element) {
+struct array *array_append(struct array *arr, const void *new_element) {
 	void *tmp;
+	if(!arr || !new_element) { return NULL; }
 	if (arr->capacity <= arr->length) {
 		if(!arr->capacity) { arr->storage = NULL; }
-		arr->capacity += 1;
+		arr->capacity += 2 * arr->capacity + 1;
 		tmp = realloc(arr->storage, arr->capacity * arr->element_size);
 		if(!tmp) { return tmp; }
 		arr->storage = tmp;
@@ -28,7 +29,7 @@ struct go_array *go_append(struct go_array *arr, const void *new_element) {
 }
 
 /**
- * \brief change the capacity of go_array
+ * \brief change the capacity of array
  *
  * if capacity = 0 its equivalent to free storage
  * if something go wrong storage its untouched
@@ -37,8 +38,10 @@ struct go_array *go_append(struct go_array *arr, const void *new_element) {
  * \param capacity of array
  * \return null if error else pointer to arr
  */
-struct go_array* go_make(struct go_array* arr, const unsigned int capacity) {
+struct array* array_make(struct array* arr, const unsigned int capacity) {
 	void *tmp;
+	if(!arr) { return NULL; }
+	if(arr->capacity == capacity) { return arr; }
 	tmp = realloc(arr->capacity ? arr->storage : NULL, capacity * arr->element_size);
 	if(!tmp) { return tmp; }
 	arr->storage = tmp;
@@ -54,8 +57,9 @@ struct go_array* go_make(struct go_array* arr, const unsigned int capacity) {
  * \param memory for copy the elem
  * \return copy of the last element, if no element return null
  */
-void* go_pop(struct go_array* arr, void* elem) {
+void* array_pop(struct array* arr, void* elem) {
 
+	if(!arr) { return NULL; }
 	if(arr->length <= 0) { return NULL; }
 
 	--arr->length;
